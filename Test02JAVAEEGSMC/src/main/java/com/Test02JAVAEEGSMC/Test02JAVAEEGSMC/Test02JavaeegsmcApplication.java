8c6b77ee699cef8e5a7aed42f0087de902931314
German Steven Méndez Clavel <clavelsteven441@gmail.com>
2024-08-09 20:12:13 -0600

    inicial

---
+++ Test02JAVAEEGSMC/src/main/java/com/Test02JAVAEEGSMC/Test02JAVAEEGSMC/Test02JavaeegsmcApplication.java
@@ -0,0 +1,13 @@
+package com.Test02JAVAEEGSMC.Test02JAVAEEGSMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test02JavaeegsmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test02JavaeegsmcApplication.class, args);
+	}
+
+}
---
+++ Test02JAVAEEGSMC/src/test/java/com/Test02JAVAEEGSMC/Test02JAVAEEGSMC/Test02JavaeegsmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Test02JAVAEEGSMC.Test02JAVAEEGSMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test02JavaeegsmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
